8f9cc02c49e3fc8a2ecd3850c0a6a60e63e053e9
DevKind89 <129765669+DevKind89@users.noreply.github.com>
2023-04-03 22:29:44 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DevHelper03
-dev helper
+
+A description of this package.
---
+++ Tests/DevHelper03Tests/DevHelper03Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DevHelper03
+
+final class DevHelper03Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DevHelper03().text, "Hello, World!")
+    }
+}
